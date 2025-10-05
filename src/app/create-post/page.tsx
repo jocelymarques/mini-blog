62a6f7d500e992f892280bd8f0a3d757f82384fe
Jocely Marques <jocelymarques1@gmail.com>
2025-10-05 19:07:51 -0300

    fix: adjustment of variables

--- src/app/create-post/page.tsx
+++
@@ -1,9 +0,0 @@
-const CreatePostPage = () => {
-  return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-2xl font-bold mb-4">Criar Novo Post</h1>
-    </main>
-  );
-};
-
-export default CreatePostPage;
--- src/app/firebase/config.ts
+++ src/app/firebase/config.ts
@@ -5,13 +5,13 @@ import { getAuth, Auth } from "firebase/auth";
 import { FirebaseApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1fiI4pUOFgaMTkVHYCf5kLdBqFspvowo",
-  authDomain: "mini-blog-95c33.firebaseapp.com",
-  projectId: "mini-blog-95c33",
-  storageBucket: "mini-blog-95c33.firebasestorage.app",
-  messagingSenderId: "768656311849",
-  appId: "1:768656311849:web:6da6b41639873bb6063e38",
-  measurementId: "G-PR21XHV2VF"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
